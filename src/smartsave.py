9239aaf9d256f4ac41d2014a39045033bcb6ebbb
Steven <thevgm01@gmail.com>
2020-10-12 13:18:16 -0500

    Added a grid with Descriptor/Task/Version info

--- src/smartsave.py
+++ src/smartsave.py
@@ -32,10 +32,25 @@ class SmartSaveUI(QtWidgets.QDialog):
         self.title_label = QtWidgets.QLabel("Smart Save")
         self.title_label.setStyleSheet("font: bold 20px")
         self.folder_layout = self._create_folder_ui()
+        self.filename_layout = self._create_filename_ui()
         self.main_layout = QtWidgets.QVBoxLayout()
         self.main_layout.addWidget(self.title_label)
         self.main_layout.addLayout(self.folder_layout)
+        self.main_layout.addLayout(self.filename_layout)
         self.setLayout(self.main_layout)
+
+    def _create_filename_ui(self):
+        self.descriptor_header_label = QtWidgets.QLabel("Descriptor")
+        self.descriptor_header_label.setStyleSheet("font: bold")
+        self.task_header_label = QtWidgets.QLabel("Task")
+        self.task_header_label.setStyleSheet("font: bold")
+        self.version_header_label = QtWidgets.QLabel("Version")
+        self.version_header_label.setStyleSheet("font: bold")
+        layout = QtWidgets.QGridLayout()
+        layout.addWidget(self.descriptor_header_label, 0, 0)
+        layout.addWidget(self.task_header_label, 0, 2)
+        layout.addWidget(self.version_header_label, 0, 4)
+        return layout
 
     def _create_folder_ui(self):
         default_folder = Path(cmds.workspace(rootDirectory=True, query=True))

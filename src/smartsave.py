b17069b6b877392f153a78f037eba0afdaf94457
Steven <thevgm01@gmail.com>
2020-10-12 13:01:11 -0500

    Added file path input and browse button

--- src/smartsave.py
+++ src/smartsave.py
@@ -30,8 +30,14 @@ class SmartSaveUI(QtWidgets.QDialog):
     def create_ui(self):
         self.title_label = QtWidgets.QLabel("Smart Save")
         self.title_label.setStyleSheet("font: bold 20px")
+        self.folder_line_edit = QtWidgets.QLineEdit("C:\\")
+        self.folder_browse_button = QtWidgets.QPushButton("...")
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.folder_line_edit)
+        layout.addWidget(self.folder_browse_button)
         self.main_layout = QtWidgets.QVBoxLayout()
         self.main_layout.addWidget(self.title_label)
+        self.main_layout.addLayout(layout)
         self.setLayout(self.main_layout)
 
 

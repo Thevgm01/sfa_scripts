4a33ef52c89e63aca4774b85dd748c7d2e74b493
Steven <thevgm01@gmail.com>
2020-11-21 15:28:45 -0600

    Added the ability to modify targets/sources separately with the UI

--- src/scatter.py
+++ src/scatter.py
@@ -1,4 +1,3 @@
-import copy
 import logging
 import random
 
@@ -28,7 +27,7 @@ class ScatterUI(QtWidgets.QDialog):
         self.scatterer = Scatterer()
         self.create_ui()
         self.create_connections()
-        self.resize(500, 100)
+        self.resize(100, 500)
         self.setMaximumWidth(700)
         self.setMaximumHeight(500)
 
@@ -36,23 +35,25 @@ class ScatterUI(QtWidgets.QDialog):
         self.title_label = QtWidgets.QLabel("Scatterer")
         self.title_label.setStyleSheet("font: bold 20px")
         self.description_label = QtWidgets.QLabel(
-            "First click on the object you want to scatter objects onto. "
-            "Then hold shift and click on the object that you want to "
-            "scatter. Change any of the settings you want to, then click "
-            "the \"Scatter\" button.")
+            "Click on the object(s) (or vertices) that you want to scatter "
+            "to, then click on the \"Set Target(s)\" button. Repeat for the "
+            "sources that you want to scatter with. Change any settings, then "
+            "click the \"Scatter\" button.")
         self.description_label.setWordWrap(True)
         self.vector_array = self._create_vector_array_ui()
         self.alignment_button = QtWidgets.QCheckBox("Align instances to "
                                                     "surface normals")
-        self.scatter_button = QtWidgets.QPushButton("Scatter")
+        self.scatter_density_layout = self._create_scatter_density_spinbox()
+        self.scatter_buttons_layout = self._create_scatter_buttons()
         self.main_layout = QtWidgets.QVBoxLayout()
         self.main_layout.addWidget(self.title_label)
         self.main_layout.addWidget(self.description_label)
         self.main_layout.addStretch()
         self.main_layout.addLayout(self.vector_array)
         self.main_layout.addWidget(self.alignment_button)
+        self.main_layout.addLayout(self.scatter_density_layout)
         self.main_layout.addStretch()
-        self.main_layout.addWidget(self.scatter_button)
+        self.main_layout.addLayout(self.scatter_buttons_layout)
         self.setLayout(self.main_layout)
         self._set_ui_properties_from_scatter()
 
@@ -100,10 +101,77 @@ class ScatterUI(QtWidgets.QDialog):
         spinbox.setValue(0)
         return spinbox
 
+    def _create_scatter_density_spinbox(self):
+        self.scatter_density_spinbox = self._create_vector_component_spinbox()
+        self.scatter_density_spinbox.setMinimum(0)
+        self.scatter_density_spinbox.setMaximum(1)
+        self.scatter_density_label = QtWidgets.QLabel("Fraction of vertices "
+                                                      "to scatter")
+
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.scatter_density_spinbox)
+        layout.addWidget(self.scatter_density_label)
+        layout.addStretch()
+        return layout
+
+    def _create_scatter_buttons(self):
+        self.set_scatter_targets_button = \
+            QtWidgets.QPushButton("Set Target(s)")
+        self.select_scatter_targets_button = \
+            QtWidgets.QPushButton("Select Target(s)")
+
+        self.set_scatter_sources_button = \
+            QtWidgets.QPushButton("Set Source(s)")
+        self.select_scatter_sources_button = \
+            QtWidgets.QPushButton("Select Source(s)")
+
+        self.targets_selected_label = QtWidgets.QLabel("0 targets selected")
+        self.targets_selected_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.sources_selected_label = QtWidgets.QLabel("0 sources selected")
+        self.sources_selected_label.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.scatter_button = QtWidgets.QPushButton("Scatter")
+        self.undo_button = QtWidgets.QPushButton("Undo")
+
+        layout = QtWidgets.QGridLayout()
+        layout.addWidget(self.set_scatter_targets_button, 0, 0)
+        layout.addWidget(self.select_scatter_targets_button, 1, 0)
+
+        layout.addWidget(self.set_scatter_sources_button, 0, 1)
+        layout.addWidget(self.select_scatter_sources_button, 1, 1)
+
+        layout.addWidget(self.targets_selected_label, 2, 0)
+        layout.addWidget(self.sources_selected_label, 2, 1)
+
+        layout.addWidget(self.scatter_button, 3, 0)
+        layout.addWidget(self.undo_button, 3, 1)
+        return layout
+
     def create_connections(self):
         """Connects Signals and Slots"""
+        self.set_scatter_targets_button.clicked.connect(
+            self._set_scatter_targets)
+        self.select_scatter_targets_button.clicked.connect(
+            self.scatterer.select_scatter_targets)
+
+        self.set_scatter_sources_button.clicked.connect(
+            self._set_scatter_sources)
+        self.select_scatter_sources_button.clicked.connect(
+            self.scatterer.select_scatter_sources)
+
         self.scatter_button.clicked.connect(self._scatter)
+        self.undo_button.clicked.connect(self.scatterer.delete_scatters)
         return
+
+    def _set_scatter_targets(self):
+        self.scatterer.set_scatter_targets()
+        self.targets_selected_label.setText(
+            str(len(self.scatterer.scatter_targets)) + " targets selected")
+
+    def _set_scatter_sources(self):
+        self.scatterer.set_scatter_sources()
+        self.sources_selected_label.setText(
+            str(len(self.scatterer.scatter_sources)) + " sources selected")
 
     def _scatter(self):
         self._set_scatter_properties_from_ui()
@@ -122,6 +190,7 @@ class ScatterUI(QtWidgets.QDialog):
             i += 1
 
         self.alignment_button.setChecked(self.scatterer.alignment)
+        self.scatter_density_spinbox.setValue(self.scatterer.scatter_density)
 
     def _set_scatter_properties_from_ui(self):
         i = 0
@@ -134,6 +203,7 @@ class ScatterUI(QtWidgets.QDialog):
             i += 1
 
         self.scatterer.alignment = self.alignment_button.checkState()
+        self.scatterer.scatter_density = self.scatter_density_spinbox.value()
 
 
 def random_between_two_vectors(vec1, vec2):
@@ -147,10 +217,11 @@ def random_between_two_vectors(vec1, vec2):
 class Scatterer(object):
 
     def __init__(self):
-        self.selected_objects = []
-        self.scatter_density = 1
+        self.scatter_targets = []
+        self.scatter_sources = []
+        self.scatter_instances = []
         self.alignment = True
-        self.scatter_percentage = 1
+        self.scatter_density = 1
 
         # Scale Min    X Y Z
         # Scale Max    X Y Z
@@ -163,30 +234,40 @@ class Scatterer(object):
         self.attribute_array[0] = [1, 1, 1]
         self.attribute_array[1] = [1, 1, 1]
 
+    def set_scatter_targets(self):
+        self.scatter_targets = pmc.ls(os=True, fl=True)
+
+    def add_scatter_targets(self):
+        self.scatter_targets.extend(pmc.ls(os=True, fl=True))
+
+    def select_scatter_targets(self):
+        pmc.select(self.scatter_targets)
+
+    def set_scatter_sources(self):
+        self.scatter_sources = pmc.ls(os=True, fl=True)
+
+    def add_scatter_sources(self):
+        self.scatter_sources.extend(pmc.ls(os=True, fl=True))
+
+    def select_scatter_sources(self):
+        pmc.select(self.scatter_sources)
+
     def scatter(self):
-        selected_objects = []
+        all_vertexes = []
+        self.scatter_instances.append([])
 
-        # Get objects currently selected
-        selected_objects = pmc.ls(os=True, fl=True)
-        scatter_targets = []
-        scatter_sources = []
+        for target in self.scatter_targets:
+            if type(target) == pmc.general.MeshVertex:
+                all_vertexes.append(target)
+            else:
+                all_vertexes.extend(target.vtx)
 
-        if type(selected_objects[0]) == pmc.general.MeshVertex:
-            i = 0
-            while type(selected_objects[i]) == pmc.general.MeshVertex:
-                scatter_targets.append(selected_objects[i])
-                i += 1
-            scatter_sources = selected_objects[i:]
-        else:
-            scatter_targets = selected_objects[0].vtx
-            scatter_sources = selected_objects[1:]
-
-        vertexes = range(len(scatter_targets))
+        vertexes = range(len(all_vertexes))
         random.shuffle(vertexes)
-        vertexes = vertexes[:int(self.scatter_percentage * len(vertexes))]
+        vertexes = vertexes[:int(self.scatter_density * len(vertexes))]
 
         for i in vertexes:
-            vertex = scatter_targets[i]
+            vertex = all_vertexes[i]
 
             # Get the average normal of the vertex
             # pmc.select(vertex, r=True)
@@ -199,7 +280,9 @@ class Scatterer(object):
             #     i += 1
             # # print(average_normal)
 
-            new_instance = pmc.instance(random.choice(scatter_sources))
+            new_instance = pmc.instance(random.choice(self.scatter_sources))
+            self.scatter_instances[-1].append(new_instance)
+
             position = pmc.pointPosition(vertex, w=True)
             pmc.move(position[0], position[1], position[2],
                      new_instance, a=True, ws=True)
@@ -237,3 +320,7 @@ class Scatterer(object):
         #self.scatterTargetsVertices.append(vertices)
 
         return
+
+    def delete_scatters(self):
+        if len(self.scatter_instances) > 0:
+            pmc.delete(self.scatter_instances.pop(-1))
